73b92f582f1cd56258c7ca1c5b8e207c505123f4
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-21 19:27:16 -0300

    alternative answer

--- python-principles-com/min_maxing/main.py
+++ python-principles-com/min_maxing/main.py
@@ -6,3 +6,6 @@ def largest_difference(param_list):
     return max_num - min_num
 
 print(largest_difference([3, 2, 3]))
+
+# def largest_difference(param_list):
+#     return max(param_list) - min(param_list)
