2a49feac8de07649037f4cd6cef8dd311f215e0a
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-21 19:26:37 -0300

    my attempting to return min_max number difference

---
+++ python-principles-com/min_maxing/main.py
@@ -0,0 +1,8 @@
+def largest_difference(param_list):
+    param_list.sort()
+    min_num = param_list[0]
+    max_num = param_list[-1]
+
+    return max_num - min_num
+
+print(largest_difference([3, 2, 3]))
