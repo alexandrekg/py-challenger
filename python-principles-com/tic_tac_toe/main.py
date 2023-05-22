3fbadf4372b797cacd67ff2619a4d24616f1969c
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-22 16:36:00 -0300

    finish

--- python-principles-com/tic_tac_toe/main.py
+++ python-principles-com/tic_tac_toe/main.py
@@ -4,9 +4,9 @@ ROW_PARSE = {
     "C": 2
 }
 
-board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-]
 
+def get_row_col(play):
+    return int(play[1]) - 1, ROW_PARSE[play[0]]
+
+
+print(get_row_col("A3"))
