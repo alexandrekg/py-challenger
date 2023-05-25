7ed83db5df3b191000f4008434e749a9904c032e
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 19:56:05 -0300

    Finishing challenge

--- hackerrank-challenges/1-month-preparation-kit/time-conversion/main.py
+++ hackerrank-challenges/1-month-preparation-kit/time-conversion/main.py
@@ -5,10 +5,16 @@ def timeConversion(s):
         removing_timechar = s.replace('AM', '')
 
     split_time = removing_timechar.split(':')
-    add_time = [str(int(split_time[0]) + 12 if 'PM' in s else - 12)]
+
+    if 'PM' in s:
+        add_time = str(int(split_time[0]) + 12)
+    else:
+        add_time = str(int(split_time[0]) - 12)
+
+    print(add_time)
     return ":".join(add_time + split_time[1:])
 
 
 if __name__ == '__main__':
-    s = '07:05:45PM'
+    s = '12:40:22AM'
     result = timeConversion(s)
