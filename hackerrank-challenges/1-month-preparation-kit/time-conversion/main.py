16c706eb151f8543df9b27461565c0d6bde255f6
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 20:04:17 -0300

    one more

--- hackerrank-challenges/1-month-preparation-kit/time-conversion/main.py
+++ hackerrank-challenges/1-month-preparation-kit/time-conversion/main.py
@@ -8,8 +8,10 @@ def timeConversion(s):
 
     if 'PM' in s:
         add_time = [str(int(split_time[0]) + 12)]
-    else:
+    elif split_time[0] == '12' and split_time[1] > '0':
         add_time = ['0' + str(int(split_time[0]) - 12)]
+    else:
+        add_time = [split_time[0]]
     result = ":".join(add_time + split_time[1:])
     return result
 
