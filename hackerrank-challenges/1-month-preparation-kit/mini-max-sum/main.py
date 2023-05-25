5263c60f457dcc375a1ed30a5cff0b439658d13e
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 19:37:00 -0300

    finish mini-max-sum challenge

--- hackerrank-challenges/1-month-preparation-kit/mini-max-sum/main.py
+++ hackerrank-challenges/1-month-preparation-kit/mini-max-sum/main.py
@@ -1,12 +1,11 @@
 
 def mini_max_sum(arr):
+    arr.sort()
     min_sum = sum(arr[:len(arr) - 1])
     max_sum = sum(arr[::-1][:len(arr) - 1])
-    print(min_sum)
-    print(max_sum)
-
+    print(min_sum, max_sum)
 
 if __name__ == '__main__':
-    arr = [1, 3, 5, 7, 9]
+    arr = [7, 69, 2, 221, 8974]
 
     mini_max_sum(arr)
