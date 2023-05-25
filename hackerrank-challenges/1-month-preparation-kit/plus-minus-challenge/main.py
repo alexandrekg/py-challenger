12e1e108b877a88c6b719367e4afe676babc5853
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 19:20:32 -0300

    finish

--- hackerrank-challenges/1-month-preparation-kit/plus-minus-challenge/main.py
+++ hackerrank-challenges/1-month-preparation-kit/plus-minus-challenge/main.py
@@ -17,12 +17,12 @@ def plusMinus(arr):
     total_zeros = len([n for n in arr if n == 0])
     negative_numbers = len([n for n in arr if n < 0])
     print(f"%0.6f" % (positive_numbers / len(arr)))
-    print(f"%0.6f" % (total_zeros / len(arr)))
     print(f"%0.6f" % (negative_numbers / len(arr)))
+    print(f"%0.6f" % (total_zeros / len(arr)))
     # Write your code here
 
 
 if __name__ == '__main__':
-    n = [1, 1, 0, -1, -1]
+    n = [1, 2, 3, -1, -2, -3, 0, 0]
 
     plusMinus(n)
