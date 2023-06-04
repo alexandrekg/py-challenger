d877de2c25d9c6b7f6adbc282a50f1b5365d4c00
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-04 13:02:27 -0300

    finish count-of-elements-in-a-matrix alternative way challenge

--- pypup/introduction-to-arrays/count-of-elements-in-a-matrix/main.py
+++ pypup/introduction-to-arrays/count-of-elements-in-a-matrix/main.py
@@ -2,4 +2,8 @@ def count_matrix(m):
     return sum([len(x) for x in m])
 
 
-print(count_matrix([[1, 2, 3], [4, 5, 6]]))
+def alternative_way_count_matrix(m):
+    return len(m) * len(m[0])
+
+
+print(alternative_way_count_matrix([[1, 2, 3], [4, 5, 6]]))
