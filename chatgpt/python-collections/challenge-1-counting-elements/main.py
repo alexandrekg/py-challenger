fdac5d92ca3be0eb5b4bd201b34df558ee7ad7de
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-09 11:17:30 -0300

    a small improvement because without using set, can be redudant

--- chatgpt/python-collections/challenge-1-counting-elements/main.py
+++ chatgpt/python-collections/challenge-1-counting-elements/main.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 def main(arg):
     str_without_w_spaces = "".join(arg.split(" "))
-    return {k: str_without_w_spaces.count(k) for k in str_without_w_spaces}
+    return {k: str_without_w_spaces.count(k) for k in set(str_without_w_spaces)}
 
 
 if __name__ == "__main__":
