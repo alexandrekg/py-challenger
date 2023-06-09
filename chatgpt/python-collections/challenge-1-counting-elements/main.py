34459caa80f4ff57a14bf1d231917effc18d8e28
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-06-09 11:15:40 -0300

    my attempt to solve the challenge

--- chatgpt/python-collections/challenge-1-counting-elements/main.py
+++ chatgpt/python-collections/challenge-1-counting-elements/main.py
@@ -1,9 +1,11 @@
-import collections
+from collections import defaultdict
 
 
-def main():
-    print('Hi')
+def main(arg):
+    str_without_w_spaces = "".join(arg.split(" "))
+    return {k: str_without_w_spaces.count(k) for k in str_without_w_spaces}
 
 
 if __name__ == "__main__":
-    main()
+    input_str = "Hello World"
+    print(main(input_str))
